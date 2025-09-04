8d43269d35c59f2463c5b87347e1a4e4799de371
Sanjana Prajapati <sanjanaprajapat7354@gmail.com>
2025-09-04 22:48:50 +0530

    Add files via upload

---
+++ SQLClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shop_Management
+{
+    internal class SQLClass
+    {
+        public string connection_string = "Server=PRAJAPATI\\SQLEXPRESS;databace=shopDB;User Id=sa;Password=26082004";
+        private SqlConnection con = new SqlConnection()
+        {
+            ConnectionString = "Server=PRAJAPATI\\SQLEXPRESS;database=shopDB;User Id=sa;Password=26082004"
+        };
+        private SqlCommand sqlcmd;
+        public SqlDataAdapter sqlda;
+        public DataSet sqlds;
+        public DataTable dt;
+        public SqlDataReader reader;
+
+        public int recordcount;
+        public void Execute_Query(string query1)
+        {
+            try 
+            {
+
+                con.Open();
+                sqlcmd = new SqlCommand(query1, con);
+                sqlda = new SqlDataAdapter(sqlcmd);
+                sqlds = new DataSet();
+                recordcount = 0;
+                recordcount = sqlda.Fill(sqlds);
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void insert_Query(string query1)
+        {
+            try
+            {
+
+                con.Open();
+                sqlcmd = new SqlCommand(query1, con);
+                sqlda = new SqlDataAdapter(sqlcmd);
+                sqlds = new DataSet();
+                recordcount = 0;
+                recordcount = sqlda.Fill(sqlds);
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void Update(string query1)
+        {
+            try
+            {
+
+                con.Open();
+                sqlcmd = new SqlCommand(query1, con);
+                sqlda = new SqlDataAdapter(sqlcmd);
+                sqlds = new DataSet();
+                recordcount = 0;
+                recordcount = sqlda.Fill(sqlds);
+                
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Sign_up.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shop_Management
+{
+    public partial class Sign_up : Form
+    {
+        public Sign_up()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 f2 = new Form1();
+            f2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
